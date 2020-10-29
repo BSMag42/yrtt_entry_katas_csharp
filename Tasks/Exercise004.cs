ff95f16892ff1077b1caeb9d9d8f2e4126bde04f
bsmagennis42 <bsmagennis42@gmail.com>
2020-10-29 15:54:18 +0000

    E4 Ignore punctuation passes

--- Tasks/Exercise004.cs
+++ Tasks/Exercise004.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 
 // Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
 // Leave punctuation marks untouched.
@@ -14,10 +15,25 @@ namespace TechReturners.Tasks
 
             foreach (var word in words)
             {
-                var first = word.Substring(0, 1);
-                var second = word.Substring(1, word.Length-1);
-                pigPhrase += (second + first + "ay ");
+                string firstLetter = word.Substring(0, 1);
+                string secondWord = word.Substring(1, word.Length - 1);
+                //match any word followed by !
+                string pattern = "(\\w+)(!+)$";
+
+                MatchCollection matchWords = Regex.Matches(secondWord, pattern);    
+                if (matchWords.Count > 0)
+                {
+                    foreach (Match match in matchWords)
+                    {
+                        pigPhrase += (match.Groups[1] + firstLetter + "ay" + match.Groups[2]);
+                    }
+                }
+                else 
+                {
+                    pigPhrase += (secondWord + firstLetter + "ay ");
+                }
             }
+
             pigPhrase = pigPhrase.Trim(' ');
             return pigPhrase;
         }
