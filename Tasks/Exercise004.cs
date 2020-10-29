e6741c25f80aa839ef4579050f5709e38bd69cfe
bsmagennis42 <bsmagennis42@gmail.com>
2020-10-29 14:38:02 +0000

    second test to check pig latin ok

--- Tasks/Exercise004.cs
+++ Tasks/Exercise004.cs
@@ -9,7 +9,17 @@ namespace TechReturners.Tasks
     {
         public static string PigIt(string str)
         {
-            throw new NotImplementedException();
+            string[] words = str.Split(' ');
+            string pigPhrase = "";
+
+            foreach (var word in words)
+            {
+                var first = word.Substring(0, 1);
+                var second = word.Substring(1, word.Length-1);
+                pigPhrase += (second + first + "ay ");
+            }
+            pigPhrase = pigPhrase.Trim(' ');
+            return pigPhrase;
         }
     }
 }
